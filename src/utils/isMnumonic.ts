ce84d1b874573edd4789e9e876889806a2cbdbff
VigM <vmikulis@figure.com>
2022-03-30 13:35:25 -0600

    update mnumonic check with better regex which will allow many more scenarios for pasting

--- src/utils/isMnumonic.ts
+++ src/utils/isMnumonic.ts
@@ -3,7 +3,7 @@ export const isMnumonic = (value: string, minLength: number) => {
   // If the length isn't at least 48 chars just return false
   if (value.length < 48) return false;
   // alpha beta charlie delta
-  const mnumonic = value.split(' ');
+  const mnumonic = value.match(/(?<=\d+|\b)([a-zA-Z]+)(?=\d+|\b)/gm);
   // If the length of words matches what we expect
-  return mnumonic.length === minLength ? mnumonic : false;
+  return (mnumonic && mnumonic.length === minLength) ? mnumonic : false;
 };

c007245ed34203142ed45b730519b76b8568d93d
AJ Webb <webbushka@gmail.com>
2022-04-11 10:48:18 -1000

    changes to footer

--- src/Components/FooterNav/FooterNav.tsx
+++ src/Components/FooterNav/FooterNav.tsx
@@ -13,39 +13,49 @@ const Footer = styled.footer`
   height: 103px;
   width: 100%;
   max-width: 100%;
-  padding: 20px 43px 40px 43px;
+  padding: 20px 16px;
   z-index: 100;
   box-sizing: border-box;
   justify-content: space-between;
 `;
-const NavItem = styled.div<{active?: boolean}>`
+const NavItem = styled.div<{ active?: boolean }>`
   font-size: 1.2rem;
   font-weight: 400;
   cursor: pointer;
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 33%;
   svg {
-    color: ${({ active }) => active ? '#FFFFFF' : '#464B5D' };
+    color: ${({ active }) => (active ? '#FFFFFF' : '#464B5D')};
     margin-bottom: 10px;
   }
 `;
 
-export const FooterNav:React.FC = () => {
+export const FooterNav: React.FC = () => {
   const location = window?.location?.pathname;
   const navigate = useNavigate();
 
   return (
     <Footer>
-      <NavItem active={location.includes('dashboard')} onClick={() => navigate('/dashboard')}>
+      <NavItem
+        active={location.includes('dashboard')}
+        onClick={() => navigate('/dashboard')}
+      >
         <Sprite icon={ICON_NAMES.DASHBOARD} size="1.6rem" />
         Dashboard
       </NavItem>
-      <NavItem active={location.includes('transactions')} onClick={() => navigate('/transactions')}>
+      <NavItem
+        active={location.includes('transactions')}
+        onClick={() => navigate('/transactions')}
+      >
         <Sprite icon={ICON_NAMES.TRANSACTIONS} size="1.6rem" />
         Transactions
       </NavItem>
-      <NavItem active={location.includes('profile')} onClick={() => navigate('/profile')}>
+      <NavItem
+        active={location.includes('profile')}
+        onClick={() => navigate('/profile')}
+      >
         <Sprite icon={ICON_NAMES.PROFILE} size="1.6rem" />
         Profile
       </NavItem>

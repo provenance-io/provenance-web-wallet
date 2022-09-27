4c93c6ac51b0e7a91d29d21641740dc4a6decb0e
VigM <vmikulis@figure.com>
2022-09-27 11:52:18 -0600

    asset chart will now cache the results, changing the options will be instant after initial load

--- src/utils/chart/generateStartDate.ts
+++ src/utils/chart/generateStartDate.ts
@@ -2,32 +2,40 @@ import { TimePeriodType } from 'types';
 
 export const generateStartDate = (newTimePeriod: TimePeriodType) => {
   const now = new Date();
-  // Note: Set all second and ms values to 0 to allow caching
   switch (newTimePeriod) {
     case 'MINUTE': {
       // Get the past 60 min (1 Hour)
-      const start = now.setMinutes(now.getMinutes() - 60, 0, 0);
-      return new Date(start).toISOString();
+      // Note: Set all second and ms values to 0 to allow caching
+      now.setMinutes(now.getMinutes() - 60, 0, 0);
+      return new Date(now).toISOString();
     }
     case 'HOURLY': {
       // Get the past 24 hours (1 Day)
-      const start = now.setHours(now.getHours() - 24);
-      return new Date(start).toISOString();
+      now.setHours(now.getHours() - 24);
+      // Note: Set all second and ms values to 0 to allow caching
+      now.setSeconds(0, 0);
+      return new Date(now).toISOString();
     }
     case 'DAILY': {
       // Get the past 1 week (7 Day)
-      const start = now.setHours(now.getHours() - 7 * 24);
-      return new Date(start).toISOString();
+      now.setHours(now.getHours() - 7 * 24);
+      // Note: Set all second and ms values to 0 to allow caching
+      now.setSeconds(0, 0);
+      return new Date(now).toISOString();
     }
     case 'WEEKLY': {
       // Get the past 1 Month (4 Week)
-      const start = now.setMonth(now.getMonth() - 1);
-      return new Date(start).toISOString();
+      now.setMonth(now.getMonth() - 1);
+      // Note: Set all second and ms values to 0 to allow caching
+      now.setSeconds(0, 0);
+      return new Date(now).toISOString();
     }
     case 'MONTHLY': {
       // Get the past 1 Yeah (12 Months)
-      const start = now.setFullYear(now.getFullYear() - 1);
-      return new Date(start).toISOString();
+      now.setFullYear(now.getFullYear() - 1);
+      // Note: Set all second and ms values to 0 to allow caching
+      now.setSeconds(0, 0);
+      return new Date(now).toISOString();
     }
     // case 'YEARLY': {
     //   // Get the past 12 months (1 Year)
